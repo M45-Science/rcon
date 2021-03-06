c006c479405e8f74fc1c691a5ab8db38f6ff91aa
Augusto Dwenger J <dwenger@posteo.de>
2021-03-06 16:09:18 +0100

    Split test into subtests

--- rcon_test.go
+++ rcon_test.go
@@ -40,13 +40,7 @@ func startTestServer(fn func(net.Conn, *bytes.Buffer)) (string, error) {
 			requestID = -1
 		}
 
-		b.Reset()
-		binary.Write(b, binary.LittleEndian, int32(10))
-		binary.Write(b, binary.LittleEndian, int32(requestID))
-		binary.Write(b, binary.LittleEndian, int32(typeAuthResponse))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		conn.Write(b.Bytes())
+		conn.Write(buildPackage(requestID, typeAuthResponse, []byte{}).Bytes())
 
 		if fn != nil {
 			b.Reset()
@@ -55,6 +49,45 @@ func startTestServer(fn func(net.Conn, *bytes.Buffer)) (string, error) {
 	}()
 
 	return listener.Addr().String(), nil
+}
+
+func buildStartOfPackage(size int, requestID, responseType int32, body []byte) *bytes.Buffer {
+	b := bytes.NewBuffer([]byte{})
+	binary.Write(b, binary.LittleEndian, int32(minPackageSize+size))
+	binary.Write(b, binary.LittleEndian, int32(requestID))
+	binary.Write(b, binary.LittleEndian, int32(responseType))
+	binary.Write(b, binary.LittleEndian, body)
+
+	return b
+}
+
+func buildEndOfPackage(body []byte) *bytes.Buffer {
+	b := bytes.NewBuffer([]byte{})
+	binary.Write(b, binary.LittleEndian, body)
+	binary.Write(b, binary.LittleEndian, byte(0))
+	binary.Write(b, binary.LittleEndian, byte(0))
+
+	return b
+}
+
+func buildPackage(requestID, responseType int32, body []byte) *bytes.Buffer {
+	b := bytes.NewBuffer([]byte{})
+	b.Write(buildStartOfPackage(len(body), requestID, responseType, body).Bytes())
+	b.Write(buildEndOfPackage([]byte{}).Bytes())
+	return b
+}
+
+func buildPackageWithLength(requestID, responseType int32, length int) *bytes.Buffer {
+	return buildPackage(requestID, responseType, writeSpace(length).Bytes())
+}
+
+func writeSpace(length int) *bytes.Buffer {
+	b := bytes.NewBuffer([]byte{})
+	for i := 0; i < length; i++ {
+		binary.Write(b, binary.LittleEndian, byte(' '))
+	}
+
+	return b
 }
 
 func TestAuth(t *testing.T) {
@@ -75,107 +108,83 @@ func TestAuth(t *testing.T) {
 }
 
 func TestMultipacket(t *testing.T) {
-	addr, err := startTestServer(func(c net.Conn, b *bytes.Buffer) {
-		// start packet
-		// start response
-		binary.Write(b, binary.LittleEndian, int32(10+4000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(typeResponseValue))
-		for i := 0; i < 4000; i++ {
-			binary.Write(b, binary.LittleEndian, byte(' '))
-		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		// end response
-		// start response
-		binary.Write(b, binary.LittleEndian, int32(10+4000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(typeResponseValue))
-		for i := 0; i < 2000; i++ {
-			binary.Write(b, binary.LittleEndian, byte(' '))
-		}
-		c.Write(b.Bytes())
-		// end packet
 
-		// start packet
-		b.Reset()
-		for i := 0; i < 2000; i++ {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+	t.Run("write one package at once", func(t *testing.T) {
+		addr, err := startTestServer(func(c net.Conn, b *bytes.Buffer) {
+			b.Write(buildPackageWithLength(123, typeResponseValue, 4000).Bytes())
+			c.Write(b.Bytes())
+		})
+		if err != nil {
+			t.Fatal(err)
 		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		// end response
-		// start response
-		binary.Write(b, binary.LittleEndian, int32(10+2000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(typeResponseValue))
-		for i := 0; i < 2000; i++ {
-			binary.Write(b, binary.LittleEndian, byte(' '))
-		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		// end response
-		// start response - size word is split!
-		binary.Write(b, binary.LittleEndian, int32(10+2000))
-		c.Write(b.Bytes()[:len(b.Bytes())-3])
-		// end packet
 
-		b.Reset()
-		binary.Write(b, binary.LittleEndian, int32(10+2000))
-		binary.Write(b, binary.LittleEndian, int32(123))
-		binary.Write(b, binary.LittleEndian, int32(typeResponseValue))
-		for i := 0; i < 2000; i++ {
-			binary.Write(b, binary.LittleEndian, byte(' '))
+		rc, err := Dial(addr, "blerg")
+		if err != nil {
+			t.Fatal(err)
 		}
-		binary.Write(b, binary.LittleEndian, byte(0))
-		binary.Write(b, binary.LittleEndian, byte(0))
-		// end response
-		c.Write(b.Bytes()[1:])
-		// end packet
+
+		str, _, err := rc.Read()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(str) != 4000 {
+			t.Fatal("response length not correct")
+		}
+
 	})
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	rc, err := Dial(addr, "blerg")
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("write one package body over multiple writes", func(t *testing.T) {
+		length := 4000
+		addr, err := startTestServer(func(c net.Conn, b *bytes.Buffer) {
+			c.Write(buildStartOfPackage(length, 123, typeResponseValue, writeSpace(length/2).Bytes()).Bytes())
 
-	str, _, err := rc.Read()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(str) != 4000 {
-		t.Fatal("response length not correct")
-	}
+			c.Write(buildEndOfPackage(writeSpace(length / 2).Bytes()).Bytes())
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	str, _, err = rc.Read()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(str) != 4000 {
-		t.Fatal("response length not correct")
-	}
+		rc, err := Dial(addr, "blerg")
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	str, _, err = rc.Read()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(str) != 2000 {
-		t.Fatal("response length not correct")
-	}
+		str, _, err := rc.Read()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(str) != 4000 {
+			t.Fatal("response length not correct")
+		}
 
-	str, _, err = rc.Read()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if len(str) != 2000 {
-		t.Fatal("response length not correct")
-	}
+	})
 
-	err = rc.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("write one package size over multiple writes", func(t *testing.T) {
+		length := 2000
+		addr, err := startTestServer(func(c net.Conn, b *bytes.Buffer) {
+			binary.Write(b, binary.LittleEndian, int32(length+minPackageSize))
+			c.Write(b.Bytes()[:len(b.Bytes())-3]) // send without the last three bytes
+
+			b.Reset()
+			c.Write(buildPackageWithLength(123, typeResponseValue, length).Bytes()[1:]) // skip first byte
+		})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		rc, err := Dial(addr, "blerg")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		str, _, err := rc.Read()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(str) != 2000 {
+			t.Fatal("response length not correct")
+		}
+
+	})
+
 }
